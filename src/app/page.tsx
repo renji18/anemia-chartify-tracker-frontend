e812f7612a8b380d7bc0dca0d9a24f1229a1b480
Rohit Singh <rohitsingh66604@gmail.com>
2023-10-02 11:43:13 +0530

    dev: dropdown data shown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,124 @@
 "use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { getData } from '../redux/actions';
-import { useAppDispatch } from '@/utility/type';
+import { useAppDispatch, useAppSelector } from '@/utility/type';
 import FilledLinedCharts from '@/components/FilledLineChart';
+
+interface DataItem {
+  data: {
+    "Adolescents (10 - 19 years)": String[];
+    "Children (6 - 9 years)": String[];
+    "Children (6 - 59 months)": String[];
+    "Index Value": String[];
+    "Pregnant Women": String[];
+    Rank: String[];
+    Mothers: String[];
+    District: String;
+  }[];
+  quarters: String;
+  state: String;
+}
+
+
+interface CityData {
+  "Adolescents (10 - 19 years)": String[];
+  "Children (6 - 9 years)": String[];
+  "Children (6 - 59 months)": String[];
+  "Index Value": String[];
+  "Pregnant Women": String[];
+  Rank: String[];
+  Mothers: String[];
+  District: String;
+}
+
 
 export default function Home() {
    const dispatch = useAppDispatch();
-  //  const [allData, setAllData] = useState<Array<Object>>([])
-  const [state, setState] = useState<Array<String>>([
-    "Gujarat",
-  ]);
+   const [state, setState] = useState<String[]>([]);
+   const [cityData, setCityData] = useState<DataItem['data']>([]);
+  const [city, setCity] = useState<String[]>([]);
+  const [catData, setCatData] = useState<DataItem['data']>([]);
+  const [cat, setCat] = useState<String[]>([]);
+  const [selectedCategoryData, setSelectedCategoryData] = useState<String[]>([]);
+  const [selectedCity, setSelectedCity] = useState<String | null>(null);
 
-  const [city, setCity] = useState<Array<String>>(["DANG", "PATAN"]);
+  const { data } : DataItem = useAppSelector((state:any) => state?.data);
+  console.log(data)
+  const dynamicStateValues = useMemo(() =>
+    data?.map((value: any) => value.state.toString()) || [], [data]);
 
-  const [cat, setCat] = useState<Array<String>>(["Adolescents", "Children", "Mothers", "Index Value", "Pregnant Women", "Toddlers"]);
+
+    const handleStateChange = (selectedState: String) => {
+      setSelectedCity(null);
+      const selectedData: any = data.find((item: any) => item.state === selectedState);
+      console.log(selectedData)
+      
+      if (selectedData) {
+        setCityData(selectedData.data);
+        setCity(selectedData.data.map((item : CityData) => item.District));
+      } else {
+        setCity([]);
+        setCityData([]);
+      }
+    };
+
+const handleCityChange = (selectedCity: String) => {
+  setSelectedCity(selectedCity);
+  const selectedData = cityData.find(
+    (item) => item.District === selectedCity
+  );
+
+  if (selectedData) {
+  const catKeys = Object.keys(selectedData)
+    .filter((key) => key !== "District")
+    setCat(catKeys);
+  } else {
+    setCat([]);
+  }
+};
+
+const handleCategoryChange = (selectedCategory: string) => {
+  if (selectedCity) {
+    const selectedCityData: CityData | undefined = cityData.find(
+      (item) => item.District === selectedCity
+    );
+
+    if (selectedCityData) {
+      console.log(selectedCityData)
+
+       const categoryData: string[] | undefined =
+         selectedCityData[selectedCategory];
+
+       console.log(categoryData);
+        if (categoryData !== undefined) {
+        setSelectedCategoryData(categoryData);
+      } else {
+        console.log(`Category '${selectedCategory}' not found in selectedCityData.`);
+        setSelectedCategoryData([]);
+      }
+    }
+    } else {
+      setSelectedCategoryData([]);
+    }
+};
+
 
    useEffect(() => {
-     dispatch(getData());
+    dispatch(getData());
    }, [dispatch]);
+
+   useEffect(() => {
+    setState(dynamicStateValues);
+   },[dynamicStateValues])
+
+   useEffect(() => {
+     console.log(cityData);
+   }, [cityData]);
+
+   useEffect(() => {
+     console.log(selectedCategoryData);
+   }, [selectedCategoryData]);
 
    
 
@@ -29,6 +129,7 @@ export default function Home() {
           <select
             name="state"
             id="state"
+            onChange={(e) => handleStateChange(e.target.value)}
             className="p-2 w-full rounded-md outline-none"
           >
             <option value="">Select the state</option>
@@ -48,12 +149,14 @@ export default function Home() {
           <select
             name="city"
             id="city"
-            className="p-2 w-full rounded-md outline-none"
+            disabled={!city.length}
+            onChange={(e) => handleCityChange(e.target.value)}
+            className="p-2 w-full rounded-md outline-none disabled:cursor-not-allowed"
           >
             <option value="">Select the city</option>
             {city &&
               city.map((item: String, key: number) => (
-                <option key={key} value={item.toString()}>
+                <option key={key} value={item?.toString()}>
                   {item}
                 </option>
               ))}
@@ -63,7 +166,9 @@ export default function Home() {
           <select
             name="cat"
             id="cat"
-            className="p-2 w-full rounded-md outline-none"
+            disabled={!cat.length}
+            onChange={(e) => handleCategoryChange(e.target.value)}
+            className="p-2 w-full rounded-md outline-none disabled:cursor-not-allowed"
           >
             <option value="">Select the category</option>
             {cat &&
--- src/components/FilledLineChart.tsx
+++ src/components/FilledLineChart.tsx
@@ -1,22 +1,45 @@
-import { useEffect } from "react"
+import { useEffect, useMemo, useState } from "react"
 import { Chart } from "chart.js";
+import { useAppSelector } from "@/utility/type";
+
+interface DataItem {
+  data: {
+    adolescents: String[];
+    children: String[];
+    indexValue: String[];
+    mothers: String[];
+    District: String;
+    pregnantWomen: String[];
+  }[];
+  quarters: String;
+  state: String;
+}
 
 export default function FilledLinedCharts() {
+  const [xAxis, setXAxis] = useState<String[]>([]);
+  
+  const { data }: DataItem = useAppSelector((state: any) => state?.data);
+
+
+    const dynamicXAxisValues = useMemo(
+        () =>
+      data
+        ?.map((value: any) => `${value.quarters.toString()}_I`)
+        .join(",")
+        .split(",") || [],
+    [data]
+  );
+
+    useEffect(() => (
+      setXAxis(dynamicXAxisValues)
+    ),[dynamicXAxisValues])
 
       useEffect(() => {
         var ctx = document.getElementById("myChart") as HTMLCanvasElement;
         var myChart = new Chart(ctx, {
           type: "line",
           data: {
-            labels: [
-              "Sunday",
-              "Monday",
-              "Tuesday",
-              "Wednesday",
-              "Thursday",
-              "Friday",
-              "Saturday",
-            ],
+            labels: [],
             datasets: [
               {
                 data: [86, 114, 106, 106, 107, 111, 133],
@@ -35,11 +58,13 @@ export default function FilledLinedCharts() {
       <h1 className="self-center mt-10 text-xl font-semibold capitalize ">
         6 - 59 Months
       </h1>
+          {xAxis &&
       <div className="flex mx-auto my-auto mt-4 ">
         <div className="border border-gray-400 pt-0 rounded-xl  w-full h-fit my-auto  shadow-xl">
           <canvas id="myChart"></canvas>
         </div>
       </div>
+}
     </div>
   );
 }
