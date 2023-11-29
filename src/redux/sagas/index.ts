b2754042502672f76f70026f090d3f63e4b29151
Aadarsh Jha <aadarshjha1401@gmail.com>
2023-11-29 09:31:25 +0530

    download functionality enabled

--- src/redux/sagas/index.ts
+++ src/redux/sagas/index.ts
@@ -4,10 +4,8 @@ import * as dataGenerators from "./dataSaga"
 import * as userGenerators from "./userSaga"
 
 export default function* mySaga() {
-  yield takeLatest(
-    actionTypes.GET_DATA,
-    dataGenerators.getDataSagaCall
-  )
+  yield takeLatest(actionTypes.GET_DATA, dataGenerators.getDataSagaCall)
+  yield takeLatest(actionTypes.GET_LINK_TO_DOWNLOAD_DATA, dataGenerators.downloadDataSagaCall)
   yield takeLatest(actionTypes.POST_DATA, dataGenerators.postDataSagaCall)
   yield takeLatest(actionTypes.LOGIN_USER, userGenerators.loginUserSagaCall)
   yield takeLatest(
